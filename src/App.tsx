a370f1b5f6fe62ee6315ec277dd21d3caf92b8ee
etiene <etienejames5@gmail.com>
2024-01-06 20:16:49 +0100

    Added more changes
    Signed-off-by: etiene <etienejames5@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { AppLayout } from "./components/AppRoute";
 import routes from "./constants/routes";
 import Home from "./components/Home";
-import withAuth from "./hoc/withAuth";
+// import withAuth from "./hoc/withAuth";
 
 function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
-        <Route path={routes.HOME.pathname} element={withAuth()(Home)} />
+        <Route path={routes.HOME.pathname} element={<Home />} />
       </Route>
-      {/* <Route path="*" element={<Navigate to={routes.HOME.pathname} />} /> */}
     </Routes>
   );
 }
